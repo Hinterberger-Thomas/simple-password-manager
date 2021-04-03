30863e0b4cef9d67cbda6b26ac89e871bcdb2254
Hinterberger-Thomas <[thomas.hinterberger.eu@gmail.com]>
2021-04-03 17:01:02 +0200

    testing orion

--- src/enc.rs
+++ src/enc.rs
@@ -1,10 +1,10 @@
-use openssl::encrypt::{Encrypter, Decrypter};
-use openssl::rsa::{Rsa, Padding};
-use openssl::pkey::PKey;
+use orion::*;
 
+assert_eq!(format!("The origin is: {}", origin), "The origin is: (0, 0)");
 
-fn encrypt(){
-
+pub fn encrypt(){
+    let secret_key = aead::SecretKey::default();
+    let ciphertext = aead::seal(&secret_key, "Secret message".as_bytes());
 }
 
 fn decrypt(){
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+
+mod enc;
+
 fn main() {
     println!("What do you want to do");
     println!("1) Get all of your passwords");
     println!("2) Insert a new password");
     println!("3) Generate a random password");
+    //enc::encrypt();
 }
+
+
